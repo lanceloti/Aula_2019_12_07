52bfe69f0986f2a6e8139ffcd0a9c59f82a088be
Faculdade Alfa <faculdadealfa@Mac-Prof.local>
2019-12-07 15:50:08 -0300

    Primeiro commit

---
+++ Aula_2019_12_07/Anuncios.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aula_2019_12_07
+{
+    public partial class Anuncios : ContentPage
+    {
+        public Command PopupCommand { get; set; }
+        public Command NavegarCommand { get; set; }
+        public Anuncios()
+        {
+            BindingContext = this;
+
+            NavegarCommand = new Command(Navegar);
+            PopupCommand = new Command(PopUp);
+
+            InitializeComponent();
+        }
+
+        async void Navegar()
+        {
+            Navigation.PushAsync(new ContentPage() { Title = "Página 2" });
+        }
+
+        async void PopUp()
+        {
+            Navigation.PushModalAsync(new ContentPage() { Title = "PopUp" });
+        }
+    }
+}
---
+++ Aula_2019_12_07/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aula_2019_12_07
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Command LogarCommand { get; set; }
+
+        public MainPage()
+        {
+            BindingContext = this;
+
+            Email = "leandro@lanceloti.com.br";
+            Senha = "123123";
+
+            LogarCommand = new Command(Logar);
+
+            InitializeComponent();
+
+            //Email.Text = "leandro@lanceloti.com.br";
+            //Email.BackgroundColor = Color.Red;
+            //Email.TextColor = Color.FromHex("fff");
+        }
+
+        async void Logar()
+        {
+            if (!string.IsNullOrEmpty(Email) && Email.ToLower() == "leandro@lanceloti.com.br" && !string.IsNullOrEmpty(Senha) && Senha == "123123")
+            {
+                Application.Current.MainPage = new NavigationPage(new Anuncios());
+            }
+            else
+            {
+                await DisplayAlert("Erro...", "Dados inválidos!", "Ok");
+            }
+        }
+    }
+}
